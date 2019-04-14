8192ef40a5e712e82ef18541d20bd8d90c5e0838
unknown <rmtestard@gmail.com>
2019-04-14 19:30:29 +0200

    routes setup

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Voluptate sit exercitation dolor sint officia nisi do nulla labore nulla ad sit elit. Nostrud ex non aliquip aliquip cillum ullamco. In occaecat ullamco elit ad consequat. Enim do exercitation sunt officia sint mollit. Aliqua sunt dolore pariatur veniam. Elit enim laborum do pariatur deserunt nisi incididunt amet sunt excepteur tempor fugiat excepteur fugiat.',
+            'author': 'Romain Testard',
+            'create_date': '04-25-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Voluptate sit exercitation dolor sint officia nisi do nulla labore nulla ad sit elit. Nostrud ex non aliquip aliquip cillum ullamco. In occaecat ullamco elit ad consequat. Enim do exercitation sunt officia sint mollit. Aliqua sunt dolore pariatur veniam. Elit enim laborum do pariatur deserunt nisi incididunt amet sunt excepteur tempor fugiat excepteur fugiat.',
+            'author': 'Marta Lopez',
+            'create_date': '04-20-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Voluptate sit exercitation dolor sint officia nisi do nulla labore nulla ad sit elit. Nostrud ex non aliquip aliquip cillum ullamco. In occaecat ullamco elit ad consequat. Enim do exercitation sunt officia sint mollit. Aliqua sunt dolore pariatur veniam. Elit enim laborum do pariatur deserunt nisi incididunt amet sunt excepteur tempor fugiat excepteur fugiat.',
+            'author': 'Vanessa Renaud',
+            'create_date': '04-6-2019'
+        }
+    ]
+    return articles
